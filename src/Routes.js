49c5c87833ba2eeff419bb52040a0accb81ebf20
Mingyue Sun <mingyueees@gmail.com>
2020-11-06 03:52:14 -0800

    Change BrowerRouter to HashRouter

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import MovieDetail from './components/MovieDetail/MovieDetail';
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/:id" component={MovieDetail} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
